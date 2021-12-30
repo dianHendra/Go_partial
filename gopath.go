566df32cf1e513a583c7cb2158c7f4f5fbc13665
Dian Hendra Saputra <90003181@medcoenergi.com>
2021-12-30 23:27:20 +0700

    gopath

---
+++ gopath.go
@@ -0,0 +1,6 @@
+// go path adalah lokasi pprojek dan library golang
+// jadi setiap go projek harus di dalam go path tersebut
+// nantinya setiap projek akan saling berkaitan dan dapat saling import 
+//langkah2
+// >> buka di windows mennu "Edit THe Environment variable"
+// >> di rebone Advance >> Environment variable
